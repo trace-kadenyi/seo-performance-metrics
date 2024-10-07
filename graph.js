d0d8b2b94ad4e10313ba1f78f56bedefc0123e07
Tracey Kadenyi <treykadenyi@gmail.com>
2024-10-07 14:57:58 +0300

    Update graph.js

--- graph.js
+++ graph.js
@@ -8,6 +8,7 @@ let seoMetricsTable = [
     june2024: "14,482",
     july2024: "19,192",
     august2024: "20,972",
+    september2024: "16,918",
   },
   {
     metric: "Users",
@@ -16,6 +17,7 @@ let seoMetricsTable = [
     june2024: "13,547",
     july2024: "17,278",
     august2024: "22,190",
+    september2024: "16,837",
   },
   {
     metric: "Organic Traffic",
@@ -24,6 +26,7 @@ let seoMetricsTable = [
     june2024: "9,021",
     july2024: "8,824",
     august2024: "10,876",
+    september2024: "8,600",
   },
   {
     metric: "Average Position",
@@ -32,6 +35,7 @@ let seoMetricsTable = [
     june2024: 19.5,
     july2024: 18.2,
     august2024: 18.9,
+    september2024: 22.1,
   },
   {
     metric: "Click-Through Rate",
@@ -40,6 +44,7 @@ let seoMetricsTable = [
     june2024: "2.6%",
     july2024: "3%",
     august2024: "3.2%",
+    september2024: "2.60",
   },
   {
     metric: "Referring Domains",
@@ -48,6 +53,7 @@ let seoMetricsTable = [
     june2024: 122,
     july2024: 132,
     august2024: 137,
+    september2024: 135,
   },
   {
     metric: "Domain Rating (DR)",
@@ -56,6 +62,7 @@ let seoMetricsTable = [
     june2024: 24,
     july2024: 24,
     august2024: 25,
+    september2024: 24,
   },
   {
     metric: "Bounce Rate",
@@ -64,6 +71,7 @@ let seoMetricsTable = [
     june2024: "39.8%",
     july2024: "39.8%",
     august2024: "38.4%",
+    september2024: "41.5",
   },
   {
     metric: "Time on Page",
@@ -72,6 +80,7 @@ let seoMetricsTable = [
     june2024: "1.7 min",
     july2024: "1.8 min",
     august2024: "1.7min",
+    september2024: "1.8min",
   },
   {
     metric: "Average Engagement Time",
@@ -80,6 +89,7 @@ let seoMetricsTable = [
     june2024: "1.33 min",
     july2024: "1.35 min",
     august2024: "1.32 min",
+    september2024: "1.25 min",
   },
 ];
 
@@ -96,6 +106,7 @@ function displayMatrixData(data) {
     june2024: 25,
     july2024: 25,
     august2024: 25,
+    september2024: 25,
   };
 
   // Create header row
@@ -105,7 +116,7 @@ function displayMatrixData(data) {
     columnWidths.june2024
   )} | ${"July 2024".padEnd(columnWidths.july2024)} | ${"August 2024".padEnd(
     columnWidths.august2024
-  )} |\n`;
+  )} | ${"September 2024".padEnd(columnWidths.september2024)} |\n`;
 
   outputText += `|${"-".repeat(columnWidths.metric + 2)}|${"-".repeat(
     columnWidths.source + 2
@@ -113,7 +124,7 @@ function displayMatrixData(data) {
     columnWidths.june2024 + 2
   )}|${"-".repeat(columnWidths.july2024 + 2)}|${"-".repeat(
     columnWidths.august2024 + 2
-  )}|\n`;
+  )}|${"-".repeat(columnWidths.september2024 + 2)}|\n`;
 
   // Create data rows
   data.forEach((item) => {
@@ -127,14 +138,16 @@ function displayMatrixData(data) {
       .toString()
       .padEnd(columnWidths.july2024)} | ${item.august2024
       .toString()
-      .padEnd(columnWidths.august2024)} |\n`;
+      .padEnd(columnWidths.august2024)} | ${item.september2024
+      .toString()
+      .padEnd(columnWidths.september2024)} |\n`;
     outputText += `|${"-".repeat(columnWidths.metric + 2)}|${"-".repeat(
       columnWidths.source + 2
     )}|${"-".repeat(columnWidths.may2024 + 2)}|${"-".repeat(
       columnWidths.june2024 + 2
     )}|${"-".repeat(columnWidths.july2024 + 2)}|${"-".repeat(
-      columnWidths.july2024 + 2
-    )}|\n`;
+      columnWidths.august2024 + 2
+    )}|${"-".repeat(columnWidths.september2024 + 2)}|\n`;
   });
 
   outputElement.innerHTML = outputText;
@@ -147,25 +160,25 @@ displayMatrixData(seoMetricsTable);
 
 // Sessions, users, organic traffic metrics
 const seoMetrics = {
-  labels: ["May 2024", "June 2024", "July 2024", "August 2024"],
+  labels: ["May 2024", "June 2024", "July 2024", "August 2024", "September 2024"],
   datasets: [
     {
       label: "Sessions",
-      data: [16414, 14482, 19192, 20972],
+      data: [16414, 14482, 19192, 20972, 16918],
       borderColor: "rgba(75, 192, 192, 1)",
       backgroundColor: "rgba(75, 192, 192, 0.2)",
       fill: true,
     },
     {
       label: "Users",
-      data: [15377, 13547, 17278, 22190],
+      data: [15377, 13547, 17278, 22190, 16837],
       borderColor: "rgba(153, 102, 255, 1)",
       backgroundColor: "rgba(153, 102, 255, 0.2)",
       fill: true,
     },
     {
       label: "Organic Traffic",
-      data: [8356, 9021, 8824, 10876],
+      data: [8356, 9021, 8824, 10876, 8600],
       borderColor: "rgba(255, 159, 64, 1)",
       backgroundColor: "rgba(255, 159, 64, 0.2)",
       fill: true,
@@ -176,11 +189,11 @@ const seoMetrics = {
 // Separate datasets for individual graphs
 // Referring Domains metrics
 const backlinksMetrics = {
-  labels: ["May 2024", "June 2024", "July 2024", "August 2024"],
+  labels: ["May 2024", "June 2024", "July 2024", "August 2024", "September 2024"],
   datasets: [
     {
       label: "Referring Domains",
-      data: [99, 122, 132, 137],
+      data: [99, 122, 132, 137, 135],
       borderColor: "rgba(255, 99, 132, 1)",
       backgroundColor: "rgba(255, 99, 132, 0.2)",
       fill: true,
@@ -190,11 +203,11 @@ const backlinksMetrics = {
 
 // Domain rating metrics
 const domainRatingMetrics = {
-  labels: ["May 2024", "June 2024", "July 2024", "August 2024"],
+  labels: ["May 2024", "June 2024", "July 2024", "August 2024", "September 2024"],
   datasets: [
     {
       label: "Domain Rating (DR)",
-      data: [24, 24, 24, 25],
+      data: [24, 24, 24, 25, 24],
       borderColor: "rgba(54, 162, 235, 1)",
       backgroundColor: "rgba(54, 162, 235, 0.2)",
       fill: true,
@@ -204,11 +217,11 @@ const domainRatingMetrics = {
 
 // Bounce rate metrics
 const bounceRateMetrics = {
-  labels: ["May 2024", "June 2024", "July 2024", "August 2024"],
+  labels: ["May 2024", "June 2024", "July 2024", "August 2024", "September 2024"],
   datasets: [
     {
       label: "Bounce Rate",
-      data: [37.6, 39.8, 39.8, 38.4],
+      data: [37.6, 39.8, 39.8, 38.4, 41.5],
       borderColor: "rgba(255, 206, 86, 1)",
       backgroundColor: "rgba(255, 206, 86, 0.2)",
       fill: true,
@@ -218,11 +231,11 @@ const bounceRateMetrics = {
 
 // Click-through rate metrics
 const clickThroughRateMetrics = {
-  labels: ["May 2024", "June 2024", "July 2024", "August 2024"],
+  labels: ["May 2024", "June 2024", "July 2024", "August 2024", "September 2024"],
   datasets: [
     {
       label: "Click-Through Rate",
-      data: [3, 2.6, 3, 3.2],
+      data: [3, 2.6, 3, 3.2, 2.6],
       borderColor: "rgba(75, 192, 192, 1)",
       backgroundColor: "rgba(75, 192, 192, 0.2)",
       fill: true,
@@ -232,11 +245,11 @@ const clickThroughRateMetrics = {
 
 // Average position metricd
 const averagePositionMetrics = {
-  labels: ["May 2024", "June 2024", "July 2024", "August 2024"],
+  labels: ["May 2024", "June 2024", "July 2024", "August 2024", "September 2024"],
   datasets: [
     {
       label: "Average Position",
-      data: [19.6, 19.5, 18.2, 18.9],
+      data: [19.6, 19.5, 18.2, 18.9, 22.1],
       borderColor: "rgba(153, 102, 255, 1)",
       backgroundColor: "rgba(153, 102, 255, 0.2)",
       fill: true,
@@ -246,11 +259,11 @@ const averagePositionMetrics = {
 
 // Time on page metrics
 const timeOnPageMetrics = {
-  labels: ["May 2024", "June 2024", "July 2024", "August 2024"],
+  labels: ["May 2024", "June 2024", "July 2024", "August 2024", "September 2024"],
   datasets: [
     {
       label: "Time on Page",
-      data: [1.7, 1.7, 1.8, 1.7],
+      data: [1.7, 1.7, 1.8, 1.7, 1.8],
       borderColor: "rgba(255, 159, 64, 1)",
       backgroundColor: "rgba(255, 159, 64, 0.2)",
       fill: true,
@@ -259,11 +272,11 @@ const timeOnPageMetrics = {
 };
 // Average time metrics
 const averageEngagementTimeMetrics = {
-  labels: ["May 2024", "June 2024", "July 2024", "August 2024"],
+  labels: ["May 2024", "June 2024", "July 2024", "August 2024", "September 2024"],
   datasets: [
     {
       label: "Average Engagement Time",
-      data: [1.23, 1.33, 1.35, 1.32], // Decimal minutes data
+      data: [1.23, 1.33, 1.35, 1.32, 1.25], // Decimal minutes data
       borderColor: "rgba(54, 162, 235, 1)",
       backgroundColor: "rgba(54, 162, 235, 0.2)",
       fill: true,
